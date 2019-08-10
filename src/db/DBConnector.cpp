5e72f404c04b21c679b1ac77c93330cf9efd2672
Ibrahim Suleiman <ibrosule80@gmail.com>
2019-08-10 11:55:28 +0100

    redirect errors to std error, use std::cerr

--- src/db/DBConnector.cpp
+++ src/db/DBConnector.cpp
@@ -25,7 +25,7 @@ m_ptr_progradoDatabase(nullptr)
 
     if(rc != SQLITE_OK)
     {
-        std::cout << "Progrado encountered an error and has to quit: Couldn't open database\n";
+        std::cerr << "Progrado encountered an error and has to quit: Couldn't open database\n";
         exit(-1);
     }
 
