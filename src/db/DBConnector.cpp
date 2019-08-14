a2e21f0401b6fe5569987a54801ad04bba46aefc
Ibrahim Suleiman <ibrosule80@gmail.com>
2019-08-14 22:06:40 +0100

    updated DBConnector.cpp

--- src/db/DBConnector.cpp
+++ src/db/DBConnector.cpp
@@ -309,44 +309,157 @@ int DB::DBConnector::removeCourse(const Progrado::Course& t_course)
     return Progrado::SUCCESS;                 
 }
 
-std::vector< std::shared_ptr<Progrado::Course> >
-DB::DBConnector::getAllCourses()
-{
-    sqlite3_stmt* getCourseStmt = nullptr;
-    std::vector< std::shared_ptr<Progrado::Course> > r_AllCoursesVector;
 
-    sqlite3_prepare_v2(
-        m_ptr_progradoDatabase,
-        Progrado::GET_ALL_COURSES.c_str(),
-        -1,
-        &getCourseStmt,
-        nullptr);
-                
-    while (sqlite3_step(getCourseStmt) == SQLITE_ROW)
+std::vector< std::shared_ptr<Progrado::Course> >
+DB::DBConnector::getCoursesMatching(const int t_flag)
+{
+    sqlite3_stmt* getCoursesStmt = nullptr;
+
+    using namespace Progrado;
+    
+
+    switch (t_flag)
+    {
+    case(FRESHMAN_FALL):
+            sqlite3_prepare_v2(
+            m_ptr_progradoDatabase,
+            GET_FRHMN_FALL_COURSES.c_str(),
+            -1,
+            &getCoursesStmt,
+            nullptr);
+
+            break;
+
+
+    case (FRESHMAN_SPRING):
+            sqlite3_prepare_v2(
+            m_ptr_progradoDatabase,
+            GET_FRHMN_SPRG_COURSES.c_str(),
+            -1,
+            &getCoursesStmt,
+            nullptr);
+
+            break;
+
+
+    case(SOPH_FALL):
+            sqlite3_prepare_v2(
+            m_ptr_progradoDatabase,
+            GET_SOPH_FALL_COURSES.c_str(),
+            -1,
+            &getCoursesStmt,
+            nullptr);
+
+            break;
+
+
+    case(SOPH_SPRING):
+            sqlite3_prepare_v2(
+            m_ptr_progradoDatabase,
+            GET_SOPH_SPRG_COURSES.c_str(),
+            -1,
+            &getCoursesStmt,
+            nullptr);
+
+            break;
+
+
+    case(JUNIOR_FALL):
+            sqlite3_prepare_v2(
+            m_ptr_progradoDatabase,
+            GET_JUN_FALL_COURSES.c_str(),
+            -1,
+            &getCoursesStmt,
+            nullptr);
+
+            break;
+
+
+    case(JUNIOR_SPRING):
+            sqlite3_prepare_v2(
+            m_ptr_progradoDatabase,
+            GET_JUN_SPRG_COURSES.c_str(),
+            -1,
+            &getCoursesStmt,
+            nullptr);
+
+            break;
+
+
+    case(SENIOR_FALL):
+            sqlite3_prepare_v2(
+            m_ptr_progradoDatabase,
+            GET_SEN_FALL_COURSES.c_str(),
+            -1,
+            &getCoursesStmt,
+            nullptr);
+
+            break;
+
+
+    case(SENIOR_SPRING):
+            sqlite3_prepare_v2(
+            m_ptr_progradoDatabase,
+            GET_SEN_SPRG_COURSES.c_str(),
+            -1,
+            &getCoursesStmt,
+            nullptr);
+
+            break;
+
+
+    case(ALL):
+            sqlite3_prepare_v2(
+            m_ptr_progradoDatabase,
+            GET_ALL_COURSES.c_str(),
+            -1,
+            &getCoursesStmt,
+            nullptr);
+
+            break;                                          
+
+    default:
+            sqlite3_prepare_v2(
+            m_ptr_progradoDatabase,
+            GET_ALL_COURSES.c_str(),
+            -1,
+            &getCoursesStmt,
+            nullptr);
+
+            break;
+
+    } // switch (t_flag)
+
+    std::vector< std::shared_ptr<Progrado::Course> > r_CoursesVector;
+
+     while (sqlite3_step(getCoursesStmt) == SQLITE_ROW)
     {   
-        int ct = sqlite3_column_count(getCourseStmt);
+        int ct = sqlite3_column_count(getCoursesStmt);
         
 
         std::vector<std::string> l_course;
-        int l_numCredits(-1);
+        int l_numCredits(0);
         
         for( int i = 0; i < ct; i++ )
         {   
-            /* the following if assumes the last column is always Progrado::Course::numCredits
+            /* the following if statement assumes the last column is always Progrado::Course::numCredits
             sqlite3_column_decltype should be the appropriate method, but it doesn't work for
-            some reason. Will try to fix this.
-            */
+            some reason. Will try to fix this. */
+            
             if(ct - 1 == i) 
-                l_numCredits = sqlite3_column_int(getCourseStmt, i);
-            else  /* cast const unsigned char* returned by sqlite3_column_text to const char* */   
-                l_course.push_back(std::string(reinterpret_cast<const char*>(sqlite3_column_text(getCourseStmt, i))));
+                l_numCredits = sqlite3_column_int(getCoursesStmt, i);
+            else  /* cast const unsigned char* returned by sqlite3_column_text to const char* */    
+                l_course.push_back(std::string(reinterpret_cast<const char*>(sqlite3_column_text(getCoursesStmt, i))));
             
         }
 
-        r_AllCoursesVector.push_back(
+        r_CoursesVector.push_back(
         std::make_shared<Progrado::Course>(l_course, l_numCredits)
         );
     }    
 
-    return r_AllCoursesVector;
-}
+    return r_CoursesVector;
+
+
+
+} // getCoursesMatching
