739bf89f7601565aafb6da478f4b77737491cae9
Ibrahim Suleiman <ibrosule80@gmail.com>
2019-08-26 22:57:08 +0400

    implemented some scrn functions

--- src/ui/AddCourseScreen.cpp
+++ src/ui/AddCourseScreen.cpp
@@ -9,6 +9,8 @@ Copyright 2019
  */
 
 #include"AddCourseScreen.h"
+#include"Progrado.h"
+#include"Course.h"
 #include<iostream>
 
 Ui::AddCourseScreen::AddCourseScreen()
@@ -23,11 +25,25 @@ Ui::AddCourseScreen::~AddCourseScreen()
 
 void Ui::AddCourseScreen::display()
 {
-    std::cout << "Override\n";
+    std::cout << "****************\nADD COURSE SCREEN\n*******************";
+    std::cout << "Add a course to the Database here\n";
 }
 
 Progrado::Course Ui::AddCourseScreen::getInput()
-{
+{   
+    std::string name,id,sem,year,type;
+    int credits;
+
+    std::cout << "Please Enter the following information to add a new course:\n";
+    std::cout << "Course Name: "; std::getline(std::cin, name);
+    std::cout << "Course ID: "; std::getline(std::cin, id);
+    std::cout << "Semester: "; std::getline(std::cin,sem);
+    std::cout << "Year (e.g. Sophomore): "; std::getline(std::cin, year);
+    std::cout << "Course type (Major, Minor, Elective, Core): "; std::getline(std::cin, type);
+    std::cout << "Course credits: "; std::cin >> credits; 
+    std::cin.ignore();
+
+    return Progrado::Course(name,id,type, sem, year, credits);
 
 
 }
--- src/ui/LoginScreen.cpp
+++ src/ui/LoginScreen.cpp
@@ -9,6 +9,8 @@ Copyright 2019
  */
 
 #include"LoginScreen.h" 
+#include<iostream>
+#include<string>
 
 Ui::LoginScreen::LoginScreen()
 {
@@ -22,15 +24,19 @@ Ui::LoginScreen::~LoginScreen()
 
 void Ui::LoginScreen::display()
 {
-
+    std::cout << "*******LOGIN********\n";
 }
 
 std::string Ui::LoginScreen::getUserName()
 {
-    
+    std::string username;
+    std::cout << "Enter username to login: "; std::getline(std::cin, username);
+    return username;
 }
 
 std::string Ui::LoginScreen::getPassword()
 {
-
+    std::string password;
+    std::cout << "Enter password to login: "; std::getline(std::cin, password);
+    return password;
 }
